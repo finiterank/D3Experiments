55dfa9ff3bd921dab00d6899cc51ae35cd460ea9
Javier Moreno <bluelephant@gmail.com>
2014-03-06 10:34:54 -0500

    Click decide el foco de la transformación

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
 var url = "tekkon.jpg";
+var esp = 20;
 var img = new Image();
 img.src = url;
 img.onload = function(){
 	d3.select('#message').style("display", "none");
  	main();
 }
-
-// function preloader() {
-// 	console.log("Preloader():: " + url + " antes de");
-// 	img.src = url;
-// 	console.log("Preloader():: " + url + " después de");
-// }
 
 function main(){
 	var context = document.getElementById('hidden-canvas').getContext('2d');
@@ -23,7 +18,7 @@ function main(){
 
 	var w = img.width;
 	var	h = img.height;
-	var esp = 20;
+
 
 	console.log(w, h);
 
@@ -36,6 +31,16 @@ function main(){
 			var color = d3.rgb(colorrgb[0], colorrgb[1], colorrgb[2]).toString();
 			tabla.push({"x": punto[0], "y": punto[1], "color": color});
 		}
+	}
+
+
+	function distanciaCuadradaPunto(a, p){
+		return Math.pow(a.x - p.x, 2) + Math.pow(a.y - p.y, 2);
+
+	}
+
+	function orderDistancia(a, b, centro){
+		return distanciaCuadradaPunto(a, centro) - distanciaCuadradaPunto(b, centro);
 	}
 
 	var datos = {"espacio": esp, "xdim": w, "ydim": h, "tabla": tabla}
@@ -78,7 +83,7 @@ function main(){
       		.style("fill-opacity", 1)
       		.style("fill", "#000000") 
       		.style("stroke-width", 0)
-      		.on("click", transformer);
+      		.on("click", function(d){transformer(d);});
 
 	d3.select(window).on('resize', resize); 
 
@@ -100,16 +105,22 @@ function main(){
       			var col = d3.rgb(d.color);
 	    		var factor = colScale((0.3 * col.r) + (0.6 * col.g) + (0.11 * col.b));
       			return factor * xScale(ballradius);
-      		})
+      		});
 
      	d3.selectAll(".colordot")
      		.attr("cx", function(d) { return xScale(d.x); })
       		.attr("cy", function(d) { return yScale(d.y); })
-      		.attr("r", xScale(ballradius))
+      		.attr("r", xScale(ballradius));
 	}
 
-	function transformer(){
-		puntos.data(datos.tabla)
+	function transformer(h){
+		puntos
+			.sort(function(a,b){
+			//var centrox = esp * Math.floor(w/esp/2);
+		 	//var centroy = esp * Math.floor(h/esp/2);
+		 	var centro = {"x": h.x, "y": h.y};
+		 	return orderDistancia(a, b, centro);
+			})
 			.transition()
 			.delay(function(d,i){return i * 1;})
 			.duration(1000)
@@ -118,33 +129,22 @@ function main(){
 			.attr("r", xScale(ballradius))
 			.style("fill-opacity", 1)
 			.style("fill", function(d){ return d.color;})
-			.each("end", function(){
-				d3.select(this)
-					.on("mouseover", function(){
-      					d3.select(this)
-      						.transition()
-      						.duration(1000)
-      						.attr("r", 6 * xScale(ballradius));
-      				})
-      				.on("mouseout", function(){
-      					d3.select(this)
-      						.transition()
-      						.delay(1000)
-      						.duration(1200)
-      						.attr("r", xScale(ballradius));
-      				});
-      			});
+			// .each("end", function(){
+			// 	d3.select(this)
+			// 		.on("mouseover", function(){
+   //    					d3.select(this)
+   //    						.transition()
+   //    						.duration(1000)
+   //    						.attr("r", 6 * xScale(ballradius));
+   //    				})
+   //    				.on("mouseout", function(){
+   //    					d3.select(this)
+   //    						.transition()
+   //    						.delay(1000)
+   //    						.duration(1200)
+   //    						.attr("r", xScale(ballradius));
+   //    				});
+   //    		});
 
 	}
 }
-
-//when everything is ready
-// $(document).ready(function(){
-// 	if ( window.console && window.console.log ) {
-// 	  console.log("Init:: vamos a hacer logs!");
-// 	}
-// 	preloader();
-// 	console.log("Init:: Ya hizo el preloader");
-// 	d3.select('#message').style("display", "none");
-// 	main();
-// });
